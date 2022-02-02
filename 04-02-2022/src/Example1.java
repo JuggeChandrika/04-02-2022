cf796b61d532faa40d1bd6e72c6f4eac2dbe2ccc
Venkatesh <Venkatesh@DESKTOP-TSJK2NG>
2022-02-02 08:31:26 +0530

    first commit

---
+++ 04-02-2022/src/Example1.java
@@ -0,0 +1,17 @@
+
+public class Example1 {
+
+	public static void main(String[] args) {
+		
+int customerId =7883;
+String customerName ="jugge chandrika";
+String customerAddress = "Hyderabad";
+
+System.out.println(customerId);
+System.out.println(customerName);
+System.out.println(customerAddress);
+
+
+	}
+
+}
